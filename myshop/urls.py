01526b3882d514bffca29d91104c060de41efd01
yalexer <yalexer@yandex.ru>
2025-02-19 17:12:11 +0300

    create shop

--- myshop/urls.py
+++ myshop/urls.py
@@ -1,4 +1,4 @@
-"""goods_store URL Configuration
+"""myshop URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cart/', include('cart.urls', namespace='cart')),
+    path('orders/', include('orders.urls', namespace='orders')),
+    path('', include('shop.urls', namespace='shop')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200, verbose_name='категория')
+    slug = models.SlugField(max_length=200,
+                            unique=True)
+
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['name']),
+        ]
+        verbose_name = 'cкатегория'
+        verbose_name_plural = 'категории'
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category',
+                       args=[self.slug])
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category,
+                                 related_name='products',
+                                 on_delete=models.CASCADE, verbose_name='товар')
+    name = models.CharField(max_length=200, verbose_name='наименование')
+    slug = models.SlugField(max_length=200)
+    image = models.ImageField(upload_to='products/%Y/%m/%d',
+                              blank=True, verbose_name='фото')
+    description = models.TextField(blank=True, verbose_name='описание')
+    price = models.DecimalField(max_digits=10,
+                                decimal_places=2, verbose_name='цена')
+    available = models.BooleanField(default=True, verbose_name='наличие')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'товар'
+        verbose_name_plural = 'товары'
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['id', 'slug']),
+            models.Index(fields=['name']),
+            models.Index(fields=['-created']),
+        ]
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail',
+                       args=[self.id, self.slug])
